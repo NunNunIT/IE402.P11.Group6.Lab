594682e86b1f22cb0d12c7de61a2afae55526a5c
fredle09 <le197903@gmail.com>
2024-09-25 22:13:01 +0700

    feat(place): update place items

---
+++ lab-1/place/drink/index.js
@@ -0,0 +1,17 @@
+import {
+  POINT_TEMPLATE_SCHOOL,
+  SCHOOL_SYMBOL
+} from "../utils/constant.js";
+import datas from './data.js';
+
+const drinks = datas.map((data) => ({
+  type: "point",
+  longitude: data["location/lng"],
+  latitude: data["location/lat"],
+  Name: data.title,
+  Location: data.address,
+  symbol: SCHOOL_SYMBOL,
+  popupTemplate: POINT_TEMPLATE_SCHOOL,
+}))
+
+export default drinks;
