98876ee5c674e6f32f24da6f6c2c2429a50e4792
Nubbynatt <21521415@gm.uit.edu.vn>
2024-09-25 22:28:50 +0700

    Add Quan 7, Quan 8, Quan 2, Quan 1, Binh Thanh, Cu Chi, Quan 6, Go Vap, Phu Nhuan, Quan 12, Tan Phu

--- lab-1/districts/index.js
+++ lab-1/districts/index.js
@@ -2,10 +2,21 @@
 import CanGio from "./CanGio.js";
 import NhaBe from "./NhaBe.js";
 import BinhChanh from "./BinhChanh.js";
+import Quan7 from "./Quan7.js";
+import Quan8 from "./Quan8.js";
+import Quan2 from "./Quan2.js";
+import Quan1 from "./Quan1.js";
+import BinhThanh from "./BinhThanh.js";
+import CuChi from "./CuChi.js";
+import Quan6 from "./Quan6.js";
+import GoVap from "./GoVap.js";
+import PhuNhuan from "./PhuNhuan.js";
+import Quan12 from "./Quan12.js";
+import TanPhu from "./TanPhu.js";
 
 import { POINT_TEMPLATE_AREA } from "../utils/constant.js";
 
-const districts = [CanGio, NhaBe, BinhChanh].map(data => ({
+const districts = [CanGio, NhaBe, BinhChanh, Quan7, Quan8, Quan2, Quan1, BinhThanh, CuChi, Quan6, GoVap, PhuNhuan, Quan12, TanPhu].map(data => ({
   type: "polygon",
   popupTemplate: POINT_TEMPLATE_AREA,
   ...data,
