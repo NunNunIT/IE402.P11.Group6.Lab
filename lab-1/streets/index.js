60c37e11a4b808b224dd2050ec2acc2d3e77c9ca
fredle09 <le197903@gmail.com>
2024-09-27 03:23:08 +0700

    feat(streets): add pham-van-dong street

--- lab-1/streets/index.js
+++ lab-1/streets/index.js
@@ -1,9 +1,10 @@
-import Template from "./template.js";
+// import Template from "./template.js";
 import QL22 from "./QL22.js";
+import PhamVanDong from "./PhamVanDong.js";
 
 import { POINT_TEMPLATE_LINE } from "../utils/constant.js";
 
-const streets = [Template, QL22].map(data => ({
+const streets = [QL22, PhamVanDong].map(data => ({
   type: "polyline",
   popupTemplate: POINT_TEMPLATE_LINE,
   ...data,
