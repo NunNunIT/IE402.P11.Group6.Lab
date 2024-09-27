8acc86ff8e04c02cde67c0f347252c60c947bf52
Nubbynatt <21521415@gm.uit.edu.vn>
2024-09-27 22:21:41 +0700

    QL1A

--- lab-1/streets/index.js
+++ lab-1/streets/index.js
@@ -1,10 +1,11 @@
 // import Template from "./template.js";
 import QL22 from "./QL22.js";
 import PhamVanDong from "./PhamVanDong.js";
+import QL1A from "./QL1A.js";
 
 import { POINT_TEMPLATE_LINE } from "../utils/constant.js";
 
-const streets = [QL22, PhamVanDong].map(data => ({
+const streets = [QL22, PhamVanDong, QL1A].map(data => ({
   type: "polyline",
   popupTemplate: POINT_TEMPLATE_LINE,
   ...data,
