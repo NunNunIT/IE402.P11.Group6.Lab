f0d17bf3d971b75ca9d7c1819ef76ebb5b3d81d3
Phan Nguyen Hai Yen <pnhaiyen520@gmail.com>
2024-09-28 03:33:25 +0700

    add street and final 2 districts

--- lab-1/streets/index.js
+++ lab-1/streets/index.js
@@ -1,18 +1,19 @@
 // import Template from "./template.js";
 import QL22 from "./QL22.js";
 import PhamVanDong from "./PhamVanDong.js";
+import QL13 from "./QL13.js";
 
 import { POINT_TEMPLATE_LINE } from "../utils/constant.js";
 
-const streets = [QL22, PhamVanDong].map(data => ({
+const streets = [QL22, PhamVanDong, QL13].map((data) => ({
   type: "polyline",
   popupTemplate: POINT_TEMPLATE_LINE,
   ...data,
   symbol: {
     type: "simple-line",
     width: 2,
-    ...data.symbol
+    ...data.symbol,
   },
-}))
+}));
 
 export default streets;
